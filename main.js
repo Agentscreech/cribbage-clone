4508ec6d217cd7e881d3505517200e19845dfc50
Agentscreech <travis.walentin@gmail.com>
2016-12-10 22:10:54 -0800

    found how to draw the cards and renamed cards for ease of use

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-function Card(value, name, suit) {
+function Card(value, name, suit, rank) {
     this.value = value;
     this.name = name;
-    this.suit = suit;
+    this.suit = suit;test
+    this.rank = rank;
 }
 
 function deck() {
-    this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+    this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+    this.suits = ['hearts', 'diamonds', 'spades', 'clubs'];
     var cards = [];
 
     for (var s = 0; s < this.suits.length; s++) {
         for (var n = 0; n < this.names.length; n++) {
             if (n < 10) {
-                cards.push(new Card(n + 1, this.names[n], this.suits[s]));
+                cards.push(new Card(n + 1, this.names[n], this.suits[s], n + 1));
             } else {
-                cards.push(new Card(10, this.names[n], this.suits[s]));
+                cards.push(new Card(10, this.names[n], this.suits[s], n + 1));
             }
         }
     }
@@ -34,3 +35,6 @@ function shuffle(array) {
 
     return array;
 }
+
+
+$('#p1c0').append('<img src="img/cards/' + test[0].rank + '_of_' + test[0].suit +'.png">'); //this draws the target card
